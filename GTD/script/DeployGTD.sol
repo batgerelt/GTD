e43bd91ac67a2efc317f00286247302365e39887
Batgerelt <grey@uniqsoft.tech>
2024-06-08 16:44:28 +0800

    Update DeployGTD.sol

--- GTD/script/DeployGTD.sol
+++ GTD/script/DeployGTD.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
 import "../src/GTD.sol";
+import "forge-std/console.sol";
+
 
 contract DeployGTDScript is Script {
     function run() external {
@@ -12,7 +14,13 @@ contract DeployGTDScript is Script {
         GTD token = new GTD("GTD Token", "MTK", 1000000 * 10 ** 18);
 
         console.log("Contract deployed at:", address(token));
+        console.log("Balance of Deployer:", token.balanceOf(address(this)));  // Corrected to check deployer's balance
 
+
+        // GTD newAddress = new GTD("GTD Token", "MTK", 0);
+
+        // console.log("Contract deployed at zero:", address(newAddress));
+        // console.log("Balance of Deployer:", newAddress.balanceOf(address(newAddress)));
 
         vm.stopBroadcast();
     }
